9424be0bdd867b6dc4eb326be28c8dd68a833f94
qasimali <qasimaly18@gmail.com>
2023-12-25 13:59:08 +0500

    dashboard view is ready in only html

---
+++ dashboard.php
@@ -0,0 +1,134 @@
+
+		<?php
+		 include ('header.php');
+		 checkUser();
+		?>
+
+		<!--*******************
+            Preloader start
+        ********************-->
+   	 		<div id="preloader">
+				<div class="waviy">
+				<span style="--i:1">L</span>
+				<span style="--i:2">o</span>
+				<span style="--i:3">a</span>
+				<span style="--i:4">d</span>
+				<span style="--i:5">i</span>
+				<span style="--i:6">n</span>
+				<span style="--i:7">g</span>
+				<span style="--i:8">.</span>
+				<span style="--i:9">.</span>
+				<span style="--i:10">.</span>
+				</div>
+    		</div>
+		<!--*******************
+			Preloader end
+		********************-->		
+
+		<!--**********************************
+            Header start
+        ***********************************-->
+        <div class="header">
+            <div class="header-content">
+                <nav class="navbar navbar-expand">
+                    <div class=" justify-content-between">
+                        <div class="header-left">
+							<div class="dashboard_bar">
+                                Dashboard 
+                            </div>
+                        </div>
+                    </div>
+				</nav>
+			</div>
+		</div>
+        <!--**********************************
+            Header end ti-comment-alt
+        ***********************************-->
+
+		<!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+            <!-- row -->
+			<div class="container-fluid">
+				
+					<div class="col-xl-12 col-xxl-12">
+						<div class="row">
+							<div class="col-md-4" st>
+								<div class="card progress-card">
+									<div class="card-body d-flex">
+										<div class="me-auto">
+											<h4 class="card-title">Total Transactions</h4>
+											<div class="d-flex align-items-center">
+												<h2 class="fs-38 mb-0">98k</h2>
+											</div>
+										</div>		
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<div class="card-body">
+										<h4 class="card-title">Invoice Remaining</h4>
+										<div class="d-flex align-items-center">
+											<h2 class="fs-38">854</h2>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<div class="card-body">
+										<h4 class="card-title">Invoice Remaining</h4>
+										<div class="d-flex align-items-center">
+											<h2 class="fs-38">854</h2>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+							
+							<div class="col-sm-4">
+								<div class="card">
+									<div class="card-body">
+										<h4 class="card-title mt-2">Invoice Sent</h4>
+										<div class="d-flex align-items-center mt-3 mb-2">
+											<h2 class="fs-38 mb-0 me-3">456</h2>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="card">
+									<div class="card-body">
+										<h4 class="card-title mt-2">Invoice Compeleted</h4>
+										<div class="d-flex align-items-center mt-3 mb-2">
+											<h2 class="fs-38 mb-0 me-3">1467</h2>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="card">
+									<div class="card-body">
+										<h4 class="card-title mt-2">Invoice Compeleted</h4>
+										<div class="d-flex align-items-center mt-3 mb-2">
+											<h2 class="fs-38 mb-0 me-3">1467</h2>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+		
+		<?php
+		 include ('footer.php');
+		?>
+		
+        
