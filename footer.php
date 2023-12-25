6b82a095b0d268e5ed72a4450db1c8bc843b9eab
qasimali <qasimaly18@gmail.com>
2023-12-25 14:01:13 +0500

    project footer details file. this file add different file example in dashboard,category,expense,reporting etc

---
+++ footer.php
@@ -0,0 +1,35 @@
+       <!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+		
+            <div class="copyright">
+                <p>© Designed &amp; by <a href="#" target="_blank">Programmer</a> 2023</p>
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
+
+		
+
+
+	</div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="vendor/global/global.min.js"></script>
+	<!-- Dashboard 1 -->
+	<script src="js/dashboard/dashboard-1.js"></script>
+
+    <script src="js/custom.min.js"></script>
+	<script src="js/dlabnav-init.js"></script>
+	<script src="js/demo.js"></script>
+</body>
+
+</html>
