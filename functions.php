5efa11b508f07ad57983621e56eed1c424754966
qasim ali <qasimaly18@gmail.com>
2023-12-29 09:36:34 +0500

    getCategory() function update (add the dropdaown in function)

--- functions.php
+++ functions.php
@@ -29,15 +29,20 @@ function checkUser(){
        redirect('login.php');
     }
 }
-function getCategory(){
+
+function getCategory($category_id){
     global $con;    
     $res = mysqli_query($con,"select * from category order by name asc");
 
-    $html = '<select required name="category_id">';
+    $html = '<select required name="category_id">';     
     $html .= '<option value="">Select Category</option>';
-            while($row = mysqli_fetch_assoc($res)){
+    while($row = mysqli_fetch_assoc($res)){
+        if($category_id > 0 && $category_id == $row['id']){
+            $html .= '<option value="'.$row['id'].'" selected>'.$row['name'].'</option>';
+        } else {
             $html .= '<option value="'.$row['id'].'">'.$row['name'].'</option>';
-            }
+        }
+    }
     $html .= '</select>';
     return $html;
 }
