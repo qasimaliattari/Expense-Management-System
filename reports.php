c65b012f10c3ee6ac2929a9241a2db1a9971cdcd
qasim ali <qasimaly18@gmail.com>
2023-12-30 13:59:24 +0500

    create the new file reports.php and build the functionality for reporting

---
+++ reports.php
@@ -0,0 +1,109 @@
+<?php
+include('header.php');
+checkUser();
+$cat_id = '';
+$sub_sql = '';
+$from = '';
+$to = '';
+if(isset($_GET['category_id']) && $_GET['category_id']>0){
+
+  $cat_id = get_safe_value($_GET['category_id']);
+  $sub_sql = "and category.id = $cat_id";
+  $from = get_safe_value($_GET['from']);
+  $to = get_safe_value($_GET['to']);
+}
+
+$res = mysqli_query($con,"select 
+                          sum(expense.price) as price , category.name from expense,category
+                          where expense.category_id = category.id $sub_sql
+                          group by expense.category_id");
+
+?>
+
+       <!--*******************
+            Preloader start
+        ********************-->
+<div id="preloader">
+  <div class="waviy">
+    <span style="--i: 1">L</span>
+    <span style="--i: 2">o</span>
+    <span style="--i: 3">a</span>
+    <span style="--i: 4">d</span>
+    <span style="--i: 5">i</span>
+    <span style="--i: 6">n</span>
+    <span style="--i: 7">g</span>
+    <span style="--i: 8">.</span>
+    <span style="--i: 9">.</span>
+    <span style="--i: 10">.</span>
+  </div>
+</div>
+    <!--*******************
+			Preloader end
+		********************-->
+
+       <!--**********************************
+            Header start
+        ***********************************-->
+<div class="header">
+  <div class="header-content">
+    <nav class="navbar navbar-expand">
+      <div class="justify-content-between">
+        <div class="header-left">
+          <div class="dashboard_bar">Reports</div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</div>
+        <!--**********************************
+            Header end ti-comment-alt
+        ***********************************-->
+
+<div class="container">
+  <form action = "get">
+    <input type="date" name="from" required value="<?php echo $from ?>">
+    <input type="date" name="to" required value="<?php echo $to ?>"><br>
+    <?php echo getCategory( $cat_id , 'reports') ?>
+    <button  class="form_btn" name="submit">Submit</button>
+</form>
+
+<?php if(mysqli_num_rows($res)>0){  ?>
+  
+  <table class="neumorphic">
+    <thead>
+      <tr>
+        <th>Category</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+	<?php 
+    $final_price = 0;
+    while($row=mysqli_fetch_assoc($res)){
+      $final_price = $final_price + $row['price'];
+  ?>
+      <tr>
+        <td><?php echo $row['name'];?></td>
+        <td><?php echo $row['price'];?></td>
+      </tr>
+	<?php  
+    } ?>
+	 <tr>
+        <th>Total</th>
+        <th><?php echo $final_price ?></th>
+      </tr>
+    </tbody>
+  </table>
+  <?php }
+     else{
+		echo "no data found ";
+	 }
+   ?>
+  
+</div>
+<?php
+ include ('footer.php');
+?>
+
+
+
